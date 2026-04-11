a54f2e75e940528b6071c47cfd8c81e34613323d
naira-othman-tamer <naira.othman.tamer@gmail.com>
2026-04-11 13:33:28 +0200

    Shared Token Generator

---
+++ Features/AuthModule/Shared/TokenGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace ExaminationSystem.Features.AuthModule.Shared
+{
+    public class TokenGenerator
+    {
+        //public static string GenerateAccessToken(User user, IConfiguration _configuration)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //        new Claim(ClaimTypes.NameIdentifier, user.Id),
+        //        new Claim(ClaimTypes.Email, user.Email),
+        //        new Claim(ClaimTypes.Name, user.UserName),
+        //        new Claim(ClaimTypes.Role, user.Role.ToString())
+        //    };
+
+        //    var key = new SymmetricSecurityKey(
+        //        Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+
+        //    var token = new JwtSecurityToken(
+        //        issuer: _configuration["JWT:Issuer"],
+        //        audience: _configuration["JWT:Audience"],
+        //        expires: DateTime.UtcNow.AddMinutes(15),
+        //        claims: claims,
+        //        signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        //    );
+
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+    }
+}
