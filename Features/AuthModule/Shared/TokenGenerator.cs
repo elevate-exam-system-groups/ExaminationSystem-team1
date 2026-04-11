eb109e7085aaf84a1c8e7154d1431b3551a98e61
naira-othman-tamer <naira.othman.tamer@gmail.com>
2026-04-11 20:50:02 +0200

    Solve Shadow Property

--- Features/AuthModule/Shared/TokenGenerator.cs
+++ Features/AuthModule/Shared/TokenGenerator.cs
@@ -1,3 +1,8 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 ﻿namespace ExaminationSystem.Features.AuthModule.Shared
 {
     public class TokenGenerator
