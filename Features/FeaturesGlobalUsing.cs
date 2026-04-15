b8b76b1f57b742a185701dcf3593307f11035903
Amr Mohamed Shawky <amrshawky936@gmail.com>
2026-04-15 20:59:04 +0200

    fix merge conflicts

--- Features/FeaturesGlobalUsing.cs
+++ Features/FeaturesGlobalUsing.cs
@@ -2,11 +2,7 @@
 global using ExaminationSystem.Features.Questions_OptionsModule.DTOs;
 global using MediatR;
 global using ExaminationSystem.Features.Common.Enums;
-global using ExaminationSystem.Features.Questions_OptionsModule.CheckQuizPublished;
 global using ExaminationSystem.Features.Questions_OptionsModule.CreateOptions;
-global using ExaminationSystem.Features.Questions_OptionsModule.CreateQuestion;
-global using ExaminationSystem.Features.Questions_OptionsModule.GetNextOrderIndex;
-global using ExaminationSystem.Features.Questions_OptionsModule.GetQuizStatus;
 global using MailKit.Net.Smtp;
 global using MimeKit;
 global using ExaminationSystem.Controllers.QuestionController.ViewModels;
