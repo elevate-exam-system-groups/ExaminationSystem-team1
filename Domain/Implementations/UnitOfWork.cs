c6c17c5b9e203ce24d69a21efcb693490bc668d8
naira-othman-tamer <naira.othman.tamer@gmail.com>
2026-04-16 04:00:28 +0200

    Add Transaction Managment region at UnitOfWork

--- Domain/Implementations/UnitOfWork.cs
+++ Domain/Implementations/UnitOfWork.cs
@@ -1,20 +1,52 @@
 ﻿using ExaminationSystem.Domain.Data;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace ExaminationSystem.Domain.Implementations
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly Context _dbContext;
+        private IDbContextTransaction? _transaction;
         private readonly Dictionary<Type, object> _repositories = [];
 
         public UnitOfWork(Context dbContext)
          => _dbContext = dbContext;
-        
+
 
         public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken ct = default)
         {
             return await _dbContext.Database.BeginTransactionAsync(ct);
         }
+
+        #region Transaction Management
+
+        //public async Task BeginTransactionAsync()
+        //{
+        //    _transaction = await _dbContext.Database.BeginTransactionAsync();
+        //}
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync();
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+
+        #endregion
+
         public IGeneralRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseModel
         {
             //Get the type of the entity
@@ -29,7 +61,7 @@ namespace ExaminationSystem.Domain.Implementations
             return newRepo;
         }
 
-        public Task<int> SaveChangesAsync() 
+        public Task<int> SaveChangesAsync()
             => _dbContext.SaveChangesAsync();
     }
 }
