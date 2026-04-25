024fd18b8abc18be3da549e47415593537bead88
Aya-Waheed2050 <ayawaheed7@gmail.com>
2026-04-25 17:16:20 +0300

    .

--- Controllers/StudentController/StudentDashboardController.cs
+++ Controllers/StudentController/StudentDashboardController.cs
@@ -4,31 +4,35 @@ using ExaminationSystem.Features.StudentDashboard.Orchestrator.StudentDashboard;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 
+
 namespace ExaminationSystem.Controllers.StudentController
 {
+
     [Route("api/student")]
     [ApiController]
     [Authorize(Roles = "Student")]
     public class StudentDashboardController : BaseApiController
     {
+
         private readonly IMediator _mediator;
         public StudentDashboardController(IMediator mediator)
             => _mediator = mediator;
+
 
         [HttpGet("dashboard")]
         public async Task<ActionResult<ResponseViewModel<StudentDashboardResponseVm>>> GetDashboard()
         {
             var studentId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            var result = await _mediator.Send(
-                new GetStudentDashboardOrchestrator(studentId));
 
-            var mappedResult = result.IsSuccess ?
-                RequestResult<StudentDashboardResponseVm>.Success(result.Data.ToViewModel(), result.Message)
-                : RequestResult<StudentDashboardResponseVm>.Failure(
-                    result.Message, result.requestErrorCode);
+            var result = await _mediator.Send(new GetStudentDashboardOrchestrator(studentId));
 
-            return HandleResult(mappedResult);
+            if (!result.IsSuccess)
+                return HandleResult(RequestResult<StudentDashboardResponseVm>
+                    .Failure(result.Message, result.requestErrorCode));
+
+            return HandleResult(RequestResult<StudentDashboardResponseVm>
+                .Success(result.Data!.ToViewModel(), result.Message));
         }
     }
 }
