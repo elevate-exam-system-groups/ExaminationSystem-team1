1bc07ad66f9277d9713f05dd23927b569939934b
naira-othman-tamer <naira.othman.tamer@gmail.com>
2026-04-28 07:19:00 +0300

    Update Time Handling

---
+++ Features/AttemptFeatures/AttemptTimerHandling/ResolveQuizAttemptStateOrchestrator.cs
@@ -0,0 +1,56 @@
+﻿using ExaminationSystem.Features.AttemptFeatures.SubmitQuizAttempt.Orchestrators.DTOS;
+using ExaminationSystem.Features.Common.AttemptRequests.Orchestrators;
+
+namespace ExaminationSystem.Features.AttemptFeatures.AttemptTimerHandling
+{
+    public record ResolveQuizAttemptStateOrchestrator(Guid AttemptId)
+        : IRequest<RequestResult<SubmitAttemptResultDto?>>;
+
+    public class ResolveQuizAttemptStateOrchestratorValidator : AbstractValidator<ResolveQuizAttemptStateOrchestrator>
+    {
+        public ResolveQuizAttemptStateOrchestratorValidator()
+        {
+            RuleFor(x => x.AttemptId)
+                .NotEmpty().WithMessage("Attempt ID is required");
+        }
+    }
+
+    public class ResolveQuizAttemptStateOrchestratorHandler
+        : IRequestHandler<ResolveQuizAttemptStateOrchestrator, RequestResult<SubmitAttemptResultDto?>>
+    {
+        private readonly IMediator _mediator;
+        public ResolveQuizAttemptStateOrchestratorHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<RequestResult<SubmitAttemptResultDto?>> Handle(
+          ResolveQuizAttemptStateOrchestrator request, CancellationToken cancellationToken)
+        {
+            var isSubmitted = await _mediator
+                .Send(new IsAttemptSubmittedQuery(request.AttemptId), cancellationToken);
+
+            if (isSubmitted.Data)
+                return RequestResult<SubmitAttemptResultDto?>
+                    .Failure("Quiz attempt is already submitted", RequestErrorCode.Conflict);
+
+            var isTimerExpired = await _mediator
+                .Send(new IsQuizTimerExpiredQuery(request.AttemptId), cancellationToken);
+
+            if (!isTimerExpired.Data)
+                return RequestResult<SubmitAttemptResultDto?>
+                    .Success(null);
+
+
+            var closeAttemptResult = await _mediator
+                .Send(new CompleteQuizAttemptOrchestrator(request.AttemptId, QuizAttemptStatus.TimedOut), cancellationToken);
+
+            if (!closeAttemptResult.IsSuccess)
+                return RequestResult<SubmitAttemptResultDto?>
+                    .Failure(closeAttemptResult.Message, closeAttemptResult.requestErrorCode);
+
+            return RequestResult<SubmitAttemptResultDto?>
+                .Success(closeAttemptResult.Data, "Timer has elapsed", RequestErrorCode.Gone);
+        }
+    }
+
+}
